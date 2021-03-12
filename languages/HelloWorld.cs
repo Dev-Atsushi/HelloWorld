8b9914e526745c25d415da7fde90430477330db5
The_Sushi <atsushithesushi@gmail.com>
2021-03-12 17:54:46 -0300

    Create HelloWorld.cs

---
+++ languages/HelloWorld.cs
@@ -0,0 +1,9 @@
+using System; 
+namespace HelloWorldApp { 
+    class Geeks { 
+	    static void Main(string[] args) {
+           Console.WriteLine("Hello World"); 
+	       Console.ReadKey(); 
+	    } 
+    } 
+} 
